9b117868a6d0a1764c4ac076f5c6b871d8c29b2d
Данила Цыбулько <danila.nissan15@gmail.com>
2021-10-25 21:29:37 +0300

    reworkdz

--- dz2/dz2.py
+++ dz2/dz2.py
@@ -10,23 +10,27 @@ while i > 1:
     print(i, 'bottles of beer on the wall', i, 'bottles of beer.')
     i = i - 1
     print('Take one down and pass it around,', i, 'bottle of beer on the wall')
+print('Take one down and pass it around, no more bottles of beer on the wall')
+print('Go to the store and buy some more, 99 bottles of beer on the wall')
 
 # решение через for
 
-for i in range(99, 0, -1):
-    print(i, 'bottles of beer on the wall', i, 'bottles of beer.')
-    if i == 1:
-        print('Take one down and pass it around', i, 'bottle of beer on the wall')
-    else:
-        print('Take one down and pass it around', i, 'bottles of beer on the wall')
+# for i in range(99, 0, -1):
+#     print(i, 'bottles of beer on the wall', i, 'bottles of beer.')
+#     if i == 1:
+#         print('Take one down and pass it around', i, 'bottle of beer on the wall')
+#     else:
+#         print('Take one down and pass it around', i, 'bottles of beer on the wall')
 
 
 # № 2
 
-for i in range(1, 100):
-    if i % 3 == 0 and i % 5 == 0:
+for j in range(1, 101):
+    if j % 3 == 0 and j % 5 == 0:
         print('FizzBuzz')
-    elif i % 3 == 0:
+    elif j % 3 == 0:
         print('Fizz')
-    elif i % 5 == 0:
+    elif j % 5 == 0:
         print('Buzz')
+    else:
+        print(j)
--- homework/homework3/sam.rab_2.py
+++ homework/homework3/sam.rab_2.py
@@ -1,51 +1,51 @@
-#sam.rab 2
-#ex 1
-# v1 = int(input())
-# v2 = int(input())
-#
-# def operation(v2, v1):
-#     if v1 < v2:
-#         return v1
-#     elif v2 < v1:
-#         return v2
-#     elif v2 == v1:
-#         return "v1 = v2"
-# print(operation(v2, v1))
+# sam.rab 2
+# ex 1
+v1 = int(input())
+v2 = int(input())
+
+def operation(v2, v1):
+    if v1 < v2:
+        return v1
+    elif v2 < v1:
+        return v2
+    elif v2 == v1:
+        return "v1 = v2"
+print(operation(v2, v1))
 
 #ex2
 
-# v1 = int(input())
-# v2 = int(input())
-# v3 = int(input())
-#
-# def operation(v1, v2, v3):
-#     if (v1 < v2 and v1 < v3) or (v1 < v2 and v2 == v3):
-#         return v1
-#     elif (v2 < v1 and v2 < v3) or (v2 < v1 and v1 == v3):
-#         return v2
-#     elif (v3 < v1 and v3 < v2) or (v3 < v1 and v1 == v3):
-#         return v3
-#     elif v1 == v2 and v2 == v3:
-#         return "v1 = v2 = v3"
-#
-# print(operation(v1, v2, v3))
+v11 = int(input())
+v22 = int(input())
+v33 = int(input())
+
+def operation2(v11, v22, v33):
+    if (v11 < v22 and v11 < v33) or (v11 < v22 and v22 == v33):
+        return v11
+    elif (v22 < v11 and v22 < v33) or (v22 < v11 and v11 == v33):
+        return v22
+    elif (v33 < v11 and v33 < v22) or (v33 < v11 and v11 == v33):
+        return v33
+    elif v11 == v22 and v22 == v33:
+        return "v12 = v22 = v33"
+
+print(operation2(v11, v22, v33))
 
 #ex 3
 
-v1 = int(input())
-v2 = int(input())
-v3 = int(input())
+a = int(input())
+b = int(input())
+c = int(input())
 
-def operation(v1, v2, v3):
+def operation3(a, b, c,):
     count = 0
-    if v1 == v2 and v2 == v3:
+    if a == b and b == c:
         count += 3
         return count
-    elif (v1 != v2 and v2 == v3) or (v1 == v2 and v2 != v3):
+    elif (a != b and b == c) or (a == b and b != c):
         count += 2
         return count
-    elif v1 != v2 and v2 != v3:
+    elif a != b and b != c:
         count += 0
         return count
 
-print(operation(v1, v2, v3))
+print(operation3(a, b, c))
